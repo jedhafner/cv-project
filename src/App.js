78929a05ac227b2d8bf2434853890370089478e2
Jed Hafner <jedhafner@gmail.com>
2022-12-01 14:46:31 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        this is the header
+      </header>
+    </div>
+  );
+}
+
+export default App;
